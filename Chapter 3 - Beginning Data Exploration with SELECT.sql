5826fae1c906d73341eb63bfc27d7926cc6d5682
Laimis Lisauskas <laimislux@outlook.com>
2026-02-04 12:54:13 +0000

    Finished chapter
    Finished chapter with all of the exercises

--- Chapter 3 - Beginning Data Exploration with SELECT.sql
+++ Chapter 3 - Beginning Data Exploration with SELECT.sql
@@ -64,3 +64,44 @@ WHERE first_name ILIKE 'sam%';
 
 SELECT * FROM teachers
 WHERE school = 'Myers Middle School'
+
+SELECT *
+FROM teachers
+WHERE school = 'Myers Middle School' AND salary < 40000;
+
+SELECT *
+FROM teachers
+WHERE last_name = 'Cole'
+OR last_name = 'Bush';
+
+SELECT *
+FROM teachers
+WHERE school = 'F.D. Roosevelt HS'
+    AND (salary < 38000 OR salary > 40000);
+
+SELECT *
+FROM teachers
+WHERE school = 'F.D. Roosevelt HS'
+    AND salary < 38000 OR salary > 40000;
+
+SELECT first_name, last_name, school, hire_date, salary
+FROM teachers
+WHERE school LIKE '%Roos%'
+ORDER BY hire_date DESC;
+
+-- The school district superintendent asks for a list of teachers in each school. Write a query that lists the schools in alphabetical order along with teachers ordered by last name A–Z.
+
+SELECT first_name, last_name, school
+FROM teachers
+ORDER BY school ASC, last_name ASC;
+
+-- Write a query that finds the one teacher whose first name starts with the letter S and who earns more than $40,000. 
+SELECT *
+FROM teachers
+WHERE first_name LIKE 'S%' AND salary > 40000;
+
+-- Rank teachers hired since January 1, 2010, ordered by highest paid to lowest.
+SELECT *
+FROM teachers
+WHERE hire_date > '01-01-2010'
+ORDER BY salary DESC;
